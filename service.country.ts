ce461b0726d40bc378f5ed221da7e18f0960a452
eswaribala <Parameswaribala@gmail.com>
2019-10-22 15:03:07 +0530

    Update service.country.ts

--- service.country.ts
+++ service.country.ts
@@ -1,85 +1,40 @@
-import {Injectable} from "@angular/core";
+import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs/Observable";
-import {State} from "../model/model.state";
-import {ERICUSER} from "../model/model.ericuser";
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/empty';
-import 'rxjs/add/operator/retry';
-//import {stateData} from "../model/model.statedata";
+import {Observable,of as observableOf} from "rxjs/index";
+import {EMPTY} from "rxjs/index";
+import{pipe} from "rxjs/index";
+import{throwError} from "rxjs/index";
+import { catchError } from 'rxjs/operators';
 
 const httpOptions = {
-    headers: new HttpHeaders({
-        'Content-Type':  'application/json'
-    })
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
 };
-@Injectable()
-export class CountryService{
+@Injectable({
+  providedIn: 'root'
+})
+export class AddPolicyHolderService {
+
+  constructor(private httpClient:HttpClient) {
 
 
-    constructor(private http:HttpClient)
-    {
+  }
 
-    }
-
-    getAllCountries():Observable<any>
-    {
-       return this.http.get<any>("https://restcountries.eu/rest/v2/all");
-
-    }
-    getAllStatesByCode(state:State):Observable<any>
-    {
-        //return stateData.filter(obj=>obj.CountryCode===code);
-
-        return this.http.post<State>('http://localhost:6060/getStateByCode',
-            state, httpOptions);
-
-    }
-
-    addUser(user:ERICUSER):Observable<any>
-    {
-        //return stateData.filter(obj=>obj.CountryCode===code);
-
-        return this.http.post<State>('http://localhost:6060/addUser',
-            user, httpOptions)
-            .catch((err: HttpErrorResponse) => {
-
-                if (err.error instanceof Error) {
-                    // A client-side or network error occurred. Handle it accordingly.
-                    console.error('An error occurred:', err.error.message);
-                } else {
-                    // The backend returned an unsuccessful response code.
-                    // The response body may contain clues as to what went wrong,
-                    console.error(`Backend returned code ${err.status}, body was: ${err.error}`);
-                }
-
-
-                return Observable.empty<any>();
-            });
-
-    }
-
-
-
-    getAllUsers():Observable<any>
-    {
-        return this.http.get<any>("http://localhost:6060/getAllUsers");
-
-    }
-
-
-
-
-
-
-
-
-
-
-    private handleErrorObservable (error: Response | any) {
-        console.error(error.message || error);
-        return Observable.throw(error.message || error);
-    }
+  addPolicyHolder(policyHolder:any):Observable<any>
+  {
+     return this.httpClient.post('https://daimlerrpscustomer-cf.cfapps.io/addPolicyHolder',
+       policyHolder,httpOptions)
+       .pipe(
+         catchError( err => {
+           if ((err.status > 400)&&(err.status < 500)) {
+              return EMPTY;
+           } else {
+             return throwError(err);
+           }
+         })
+       );
+  }
 
 
 }
